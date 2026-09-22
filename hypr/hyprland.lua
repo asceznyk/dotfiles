ab9e9549637d6ff07ddfdc58c252a8927ede9216
asceznyk <asceznyk@gmail.com>
2026-09-22 14:39:12 +0530

    updated

--- hypr/hyprland.lua
+++ hypr/hyprland.lua
@@ -164,6 +164,7 @@ hl.bind(mainMod .. " + mouse_down", hl.dsp.focus({ workspace = "e+1" }))
 hl.bind(mainMod .. " + mouse_up", hl.dsp.focus({ workspace = "e-1" }))
 
 hl.bind(mainMod .. " + mouse:272", hl.dsp.window.drag(), { mouse = true })
+hl.bind(mainMod .. " + mouse:273", hl.dsp.window.resize(), {mouse = true })
 hl.bind(
   mainMod .. " + CTRL + up", 
   hl.dsp.window.resize({ x = 0, y = -15, relative = true }), { repeating = true }
